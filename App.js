b152e9b42f8237e329d8290415f02985eeb4547a
Zartab Nakhwa <nzartab@gmail.com>
2021-07-15 18:47:17 +0530

    TextInput Introduction

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
-import React from 'react';
-import { StyleSheet, Text, View ,StatusBar} from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View ,StatusBar,TextInput} from 'react-native';
 
 import { WelcomeScreen } from './app/screens/WelcomeScreen';
 import { ViewImageScreen } from './app/screens/ViewImageScreen';
@@ -14,15 +14,44 @@ import { Screen } from './app/components/Screen';
 import { Icon } from './app/components/Icon';
 import { ListItem } from './app/components/ListItem';
 import { AccountScreen } from './app/screens/AccountScreen';
-import { ListingsScreen } from './app/screens/ListingsScreen';
+//import { ListingsScreen } from './app/screens/ListingsScreen';
 
 export default function App() {
 
-
+  const [firstName,setFirstName]=useState("");
   return (
     <Screen>
+
+    <TextInput
+      placeholder="First Name"
+      style={{
+        borderBottomColor:"#000",
+        borderBottomWidth:1,
+
+      }}
+      onChangeText={(text)=>setFirstName(text)}
+      secureTextEntry
+     
     
-    <ListingsScreen></ListingsScreen>
+    >
+
+    </TextInput>
+    <Text>{firstName}</Text>
+
+    <TextInput
+    placeholder="Test"
+    style={{
+      borderBottomColor:"red",
+      borderBottomWidth:1,
+
+    }}
+    keyboardType="numeric"
+    clearButtonMode="always"
+    >
+
+    </TextInput>
+   
+    
     </Screen>
 
   );
