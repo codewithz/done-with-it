81a947f7bf540bf5312e40e6393c71a5da8ef7c7
Zartab Nakhwa <nzartab@gmail.com>
2021-07-15 19:58:00 +0530

    Modal

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,27 +1,46 @@
-import React from 'react'
-import { StyleSheet, Text, View ,TextInput, Platform} from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View ,Button,TextInput, Platform,TouchableWithoutFeedback,Modal} from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import colors from '../config/colors'
 import { AppText } from './AppText'
+import { Screen } from './Screen'
 
 
 export  function AppPicker({icon,placeholder}) {
+
+    const [modalVisible,setModalVisible]=useState(false);
     return (
-        <View style={styles.container}>
-         {icon &&  <MaterialCommunityIcons name={icon}
-                    size={20} color={colors.medium}
-                     style={styles.icon}
-         
-         />}
-           <AppText style={styles.text}>{placeholder}</AppText>
-           <MaterialCommunityIcons 
-            name="chevron-down"
-            size={20}
-            color={colors.medium}
-            style={styles.icon}
-           
-           />
-        </View>
+        <React.Fragment>
+            <TouchableWithoutFeedback 
+            onPress={()=>setModalVisible(true)}>
+            <View style={styles.container}>
+            {icon &&  <MaterialCommunityIcons 
+            name={icon}
+                        size={20} color={colors.medium}
+                        style={styles.icon}
+            
+            />}
+            <AppText 
+            style={styles.text}>{placeholder}</AppText>
+            <MaterialCommunityIcons 
+                name="chevron-down"
+                size={20}
+                color={colors.medium}
+                style={styles.icon}
+            
+            />
+            </View>
+            </TouchableWithoutFeedback>
+            <Modal 
+            visible={modalVisible}
+            animationType="slide"
+            >
+                <Screen>
+                <Button title="Close" 
+                onPress={()=>setModalVisible(false)}/>
+                </Screen>
+            </Modal>
+        </React.Fragment>
     )
 }
 
