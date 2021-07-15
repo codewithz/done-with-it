26fd970f3bd0995d7a79e1bcdaf3606078982f4f
Zartab Nakhwa <nzartab@gmail.com>
2021-07-15 19:26:48 +0530

    Switch

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React,{useState} from 'react';
-import { StyleSheet, Text, View ,StatusBar,TextInput} from 'react-native';
+import { StyleSheet, Text, View ,StatusBar,TextInput,Switch} from 'react-native';
 
 import { WelcomeScreen } from './app/screens/WelcomeScreen';
 import { ViewImageScreen } from './app/screens/ViewImageScreen';
@@ -19,11 +19,14 @@ import { AppTextInput } from './app/components/AppTextInput';
 
 export default function App() {
 
-  const [firstName,setFirstName]=useState("");
+  const [isNew,setIsNew]=useState(false);
   return (
     <Screen>
 
-    <AppTextInput placeholder="Username" icon="email"></AppTextInput>
+    <Switch 
+    value={isNew} 
+    onValueChange={newValue=>setIsNew(newValue)}
+    ></Switch>
     </Screen>
 
   );
