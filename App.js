61de46255b367454695e6e118b86c8967ce3ff05
Zartab Nakhwa <nzartab@gmail.com>
2021-07-20 22:01:29 +0530

    AppNavigator

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View ,StatusBar,TextInput,Switch,Button,Image} from '
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './app/navigation/AuthNavigator';
+import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 
 
@@ -12,7 +13,7 @@ export default function App() {
 
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AuthNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
 }
