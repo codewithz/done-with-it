d4056a66f76570787b9ca0330c815ddc4d0ab75b
Zartab Nakhwa <nzartab@gmail.com>
2021-07-23 18:59:28 +0530

    Icons on Navigation

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+
 import { AccountScreen } from "../screens/AccountScreen";
 import { ListingEditScreen } from "../screens/ListingEditScreen";
 import { ListingsScreen } from "../screens/ListingsScreen";
@@ -10,9 +13,30 @@ const Tab=createBottomTabNavigator();
 
 const AppNavigator= ()=>(
     <Tab.Navigator>
-        <Tab.Screen name="Listings" component={FeedNavigator}/>
-        <Tab.Screen name="ListingEdit" component={ListingEditScreen}/>
-        <Tab.Screen name="Account" component={AccountNavigator}/>
+        <Tab.Screen name="Listings" component={FeedNavigator}
+        options={
+            {
+                tabBarIcon:({color,size})=> 
+                <MaterialCommunityIcons name="home" color={color} size={size}/>
+            }
+        }
+        />
+        <Tab.Screen name="ListingEdit" component={ListingEditScreen}
+           options={
+            {
+                tabBarIcon:({color,size})=> 
+                <MaterialCommunityIcons name="plus-circle" color={color} size={size}/>
+            }
+        }
+        />
+        <Tab.Screen name="Account" component={AccountNavigator}
+               options={
+                {
+                    tabBarIcon:({color,size})=> 
+                    <MaterialCommunityIcons name="account" color={color} size={size}/>
+                }
+            }
+        />
     </Tab.Navigator>
 )
 
